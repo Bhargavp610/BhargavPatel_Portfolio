e36a6ba8e254b5b8afa56e18df74da4472b42d09
Bhargavp610 <bpatel06442@gmail.com>
2025-06-14 16:49:22 -0400

    Update animations.js
    scrolling animation

--- animations.js
+++ animations.js
@@ -1,17 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        if(entry.isIntersecting) {
-          entry.target.classList.add('animate-slide-up');
-          observer.unobserve(entry.target);
-        }
-      });
-    }, {
-      threshold: 0.1
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('animate-slide-up');
+        observer.unobserve(entry.target);
+      }
     });
-  
-    document.querySelectorAll('section').forEach(section => {
-      observer.observe(section);
-    });
+  }, {
+    threshold: 0.1
   });
-  
+
+  document.querySelectorAll('section').forEach(section => {
+    observer.observe(section);
+  });
+});
